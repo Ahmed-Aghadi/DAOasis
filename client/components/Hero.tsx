4f708eb018f72e5763a2f4f3ff7737ac499098fd
Suhel Kapadia <suhelkapadia2@gmail.com>
2023-03-26 15:22:03 +0530

    fin: all developemnt tasks done

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -1,25 +1,42 @@
-import {createStyles, Container, Text, Button, Group, rem} from '@mantine/core';
+import {
+    createStyles,
+    Container,
+    Text,
+    Button,
+    Group,
+    rem,
+    Center,
+    Blockquote,
+    Flex,
+    SimpleGrid,
+    Image as MantineImage
+} from '@mantine/core';
 import {motion} from 'framer-motion';
 import bgImage from "../public/logo-wo-bg.png"
+import img from "../public/img.png"
 import Image from "next/image";
-import {notifications} from "@mantine/notifications";
+import {IconBrandGithub, IconSettingsFilled} from "@tabler/icons-react";
 
 const useStyles = createStyles((theme) => ({
     hero: {
-        backgroundColor: theme.colors.blueTheme[0],
-        height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
     },
     image: {
-        position: 'relative',
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        // flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.colors.blueTheme[0],
     },
     btn: {
         position: 'absolute',
         bottom: rem(200),
-        left: rem(370),
+        left: rem(750),
     },
     btns: {
         backgroundColor: theme.colors.blueTheme[3],
@@ -32,27 +49,78 @@ const useStyles = createStyles((theme) => ({
 export function HeroTitle() {
     const {classes} = useStyles();
 
-    const notif = () => {
-        notifications.show({
-            title: "Source Code",
-            message: "This is the source code for the app",
-            autoClose: true,
-        })
-    }
-
     return (
         <div className={classes.hero}>
-            <div className={classes.image}>
-                <Image src={bgImage} alt={"logo"} width={1100} height={750}/>
-                <div className={classes.btn}>
-                    <Button onClick={notif} mx={"md"} size={"xl"} className={classes.btns}>Source Code</Button>
-                    <Button component={"a"} href={"/login"} mx={"md"} size={"xl"} className={classes.btns}>
-                        <motion.div whileHover={{scale: 1.0}} whileTap={{scale: 0.9}}>
-                            Launch App
-                        </motion.div>
-                    </Button>
-                </div>
-            </div>
+            <section className={classes.image}>
+                <Flex direction="column">
+                    <div style={{height: 600}}>
+                        <Image src={bgImage} alt={"logo"} width={1000} height={750}/>
+                    </div>
+                    <Group position="center" mb="xl" pb={60}>
+                        <Center>
+                            <Button component="a" href="https://github.com/Suhel-Kap/DAOasis"
+                                    leftIcon={<IconBrandGithub/>} target="_blank"
+                                    mx={"md"} size={"xl"} className={classes.btns}>Source Code</Button>
+                            <Button component={"a"} href={"/login"} mx={"md"} size={"xl"} className={classes.btns}>
+                                <motion.div whileHover={{scale: 1.0}} whileTap={{scale: 0.9}}>
+                                    Launch App
+                                </motion.div>
+                            </Button>
+                        </Center>
+                    </Group>
+                </Flex>
+            </section>
+            <section style={{height: 350, display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <Center m={"xl"} p={"xl"}>
+                    <Blockquote styles={(theme) => ({
+                        body: {
+                            fontSize: rem(30),
+                            fontFamily: "Arial",
+                            color: theme.colors.blueTheme[4]
+                        }
+                    })}>
+                        DAOs simplifiied, even for web2 users.
+                    </Blockquote>
+                </Center>
+            </section>
+            <section style={{
+                height: 650,
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#3304ba",
+                padding: "80px"
+            }}>
+                <Flex direction="column" w={"100%"}>
+                    <Center my="xl">
+                        <Text size={35} color={"white"}>
+                            Technologies Used
+                        </Text>
+                    </Center>
+                    <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+                        <SimpleGrid cols={3} w={"95%"}>
+                            <Center m="xs" p="xs">
+                                <MantineImage src={"/gnosis.svg"} alt={"Gnosis Safe"} width={230} height={230}/>
+                            </Center>
+                            <Center m="xs" p="xs">
+                                <MantineImage src={"/polygon.svg"} alt={"Polygon"} width={200} height={200}/>
+                            </Center>
+                            <Center m="xs" p="xs">
+                                <MantineImage src={"/optimism.svg"} alt={"Optimism"} width={200} height={200}/>
+                            </Center>
+                        </SimpleGrid>
+                        <SimpleGrid cols={2} w={"60%"} mt="xl">
+                            <Center m="xs" p="xs">
+                                <MantineImage src={"/connext.svg"} alt={"Connext"} width={200} height={200}/>
+                            </Center>
+                            <Center m="xs" p="xs">
+                                <MantineImage src={"/polybase.webp"} alt={"Polybase"} width={200} height={200}/>
+                            </Center>
+                        </SimpleGrid>
+                    </div>
+                </Flex>
+            </section>
         </div>
     );
 }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,116 +1,19 @@
 import Head from "next/head";
-import { SafeAuthKit, SafeAuthProviderType } from "@safe-global/auth-kit";
-import Login from "@/components/Login";
-import Logout from "@/components/Logout";
-import { Layout } from "@/components/Layout";
-import SafeAuthContext from "@/contexts/SafeAuthContext";
-import { useContext } from "react";
-import { HeroTitle } from "@/components/Hero";
-import axios from "axios";
+import {HeroTitle} from "@/components/Hero";
 
 export default function Home() {
-    const ctx = useContext(SafeAuthContext);
-
-    const getAllRecords = async () => {
-        // const res = await fetch("/api/polybase");
-        const res = await axios.get("/api/polybase", {
-            params: {
-                collection: "User",
-            },
-        });
-        console.log("DATA", res);
-    };
-
-    const getRecords = async () => {
-        const res = await fetch(
-            "/api/polybase/0x0de82DCC40B8468639251b089f8b4A4400022e04"
-        );
-        const data = await res.json();
-        console.log("DATA", data);
-    };
-
-    const createRecord = async () => {
-        const res = await fetch(
-            "/api/polybase/0x0de82DCC40B8468639251b089f8b4A4400022e04",
-            {
-                method: "POST",
-            }
-        );
-        const data = await res.json();
-        console.log("DATA", data);
-    };
-
-    const updateRecord = async () => {
-        const res = await fetch(
-            "/api/polybase/0x0de82DCC40B8468639251b089f8b4A4400022e04",
-            {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    name: "test",
-                    description: "test",
-                    image: "test",
-                }),
-            }
-        );
-        const data = await res.json();
-        console.log("DATA", data);
-    };
-
-    const createCollection = async () => {
-        // const res = await fetch("/api/polybase?id=0x0de82DCC40B8468639251b089f8b4A4400022e04");
-
-        // const res = await fetch("/api/polybase", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({
-        //         name: "test",
-        //     }),
-        // });
-
-        const res = await fetch("/api/polybase", {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                name: "test",
-                id: "60f1b1b0-1b1b-4b1b-1b1b-1b1b1b1b1b1b",
-            }),
-        });
-        console.log("RES", res);
-        const data = await res.json();
-        console.log("DATA", data);
-    };
-
     return (
         <>
             <Head>
                 <title>DAOasis</title>
-                <meta name="description" content="A multi-sig dao" />
+                <meta name="description" content="A multi-sig dao"/>
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <HeroTitle />
+            <HeroTitle/>
         </>
     );
 }
-
-// <Login/>
-// <Logout/>
-// <button onClick={() => console.log(ctx)}>Log Context</button>
-// {/* <button onClick={() => createCollection()}>
-//                 Create Collection
-//             </button> */}
-// <button onClick={() => getAllRecords()}>Get All Records</button>
-// <button onClick={() => getRecords()}>Get Records</button>
-// <button onClick={() => createRecord()}>Create Record</button>
-// <button onClick={() => updateRecord()}>Update Record</button>
-// <main>Hello World</main>
