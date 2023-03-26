4fbaa16ce84c711f760f4abdb83e7c43d888c9d1
Suhel Kapadia <suhelkapadia2@gmail.com>
2023-03-26 16:52:12 +0530

    fix: button text fix

--- client/pages/create-app.tsx
+++ client/pages/create-app.tsx
@@ -227,7 +227,7 @@ export default function CreateApp() {
                                     },
                                 }
                             })}>
-                        Create Proposal
+                        Create App
                     </Button>
                 </form>
             </Center>
